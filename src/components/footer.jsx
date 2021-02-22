db049b4bc3dd0b9cb76d59b605712ca1ec3143b5
Ashley Peterson <nosretepea@gmail.com>
2021-02-21 21:39:17 -0600

    Adding back in the class so the footer links will underline on hover

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,16 +7,16 @@ export default class Footer extends React.Component {
       <div className="footer w-full fixed z-1 bottom-0 p-4">
         <div className="mx-auto flex justify-center items-center w-full">
           <div className="text-lg">
-            <Link to={`/`} className="home mx-2 md:mx-4">
+            <Link to={`/`} className="footer__link home mx-2 md:mx-4">
               <span>home</span>
             </Link>
-            <Link to={`/about`} className="about mx-2 md:mx-4">
+            <Link to={`/about`} className="footer__link about mx-2 md:mx-4">
               <span>about</span>
             </Link>
-            <Link to={`/photos`} className="photos mx-2 md:mx-4">
+            <Link to={`/photos`} className="footer__link photos mx-2 md:mx-4">
               <span>photos</span>
             </Link>
-            <a className="resume mx-2 md:mx-4"
+            <a className="footer__link resume mx-2 md:mx-4"
               href="https://docs.google.com/document/d/1_qPgRIDe6HUI35ZE7yltQQK3NJYFk-iykZDMM4vYuZw/"
               rel="noreferrer"
               target="_blank">
